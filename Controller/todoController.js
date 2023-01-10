e2b5414148f3ba5128ec6e6956b02c5eaa669879
Dhrumil-Zignuts <dhrumila@zignuts.com>
2023-01-10 14:52:18 +0530

    All Error Solve and project working

--- Controller/todoController.js
+++ Controller/todoController.js
@@ -1,30 +1,42 @@
 let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
-let data = [
-    { item: 'Complete NodeJS Tutorials' },
-    { item: 'Complete todoList' },
-    { item: 'Complete the Learning Path ' }
-]
+// DataBase Connect 
+mongoose.connect('mongodb+srv://dhrumilZignuts:deepatel@zignuts-technology.zveo5bz.mongodb.net/toDoList');
 
-let urlencodedParser = bodyParser.urlencoded({extended:false});
+// Create a MongoDB Schema
+
+let todoSchema = new mongoose.Schema({
+    item: String
+})
+// Create Model for Database
+let Todo = mongoose.model('Todo', todoSchema);
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
     app.get('/todo', (req, res) => {
-        // console.log(data);
-        res.render('todo', { todos: data });
-    })
-
-    app.post('/todo', urlencodedParser, function(req,res){
-        console.log(`${ JSON.stringify(req.body)} new item added`);
-        data.push(req.body);
-        res.render('todo', { todos: data });
-    })
-
-    app.delete('/todo/:item',function(req,res){
-        console.log(`this is a dta from US, ${req.params.item}`);
-        let dataa = data.filter((todo)=>{
-            return todo.item.replace(/ /g, '') !== req.params.item;
+        // Get the data from MongoDG
+        Todo.find({},(err,data)=>{
+            if (err) throw err;
+            res.render('todo', { todos: data });
         })
-        res.render('todo', { todos: dataa });
+    })
+
+    app.post('/todo', urlencodedParser, function (req, res) {
+        // get data from views and send it to dataBase.
+        let newTodo = Todo(req.body).save((err,data)=>{
+            if(err) throw err;
+            res.render('todo', { todos: data });
+        })
+    })
+
+    app.delete('/todo/:item', function (req, res) {
+        console.log(req.params.item);
+        Todo.find({item: req.params.item.replace(/\-/g,' ').trim()}).remove((err,data)=>{
+            if(err) throw err;
+            console.log('Item is removed');
+            res.json(data);
+        })
     })
 }
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $('form').on('submit', function(){
+//   $('form').on('submit', function(){
 
-      var item = $('form input');
-      var todo = JSON.stringify({item: item.val()});
-      console.log(todo);
+//       var item = $('form input');
+//       var todo = JSON.stringify({item: item.val()});
+//       console.log(todo);
 
-      $.ajax({
-        type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
-          // location.redirect('/todo');
-          window.reload();
-          console.log(data);
-        }
-      });
+//       $.ajax({
+//         type: 'POST',
+//         url: '/todo',
+//         data: todo,
+//         success: function(data){
+//           //do something with the data via front-end framework
+//           // location.redirect('/todo');
+//           window.reload();
+//           console.log(data);
+//         }
+//       });
 
-      return false;
+//       return false;
 
-  });
+//   });
 
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-  });
+//   $('li').on('click', function(){
+//       var item = $(this).text().replace(/ /g, "-");
+//       $.ajax({
+//         type: 'DELETE',
+//         url: '/todo/' + item,
+//         success: function(data){
+//           //do something with the data via front-end framework
+//           location.reload();
+//         }
+//       });
+//   });
 
-});
+// });
